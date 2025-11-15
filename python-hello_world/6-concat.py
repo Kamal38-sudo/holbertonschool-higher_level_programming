6fd0c31b69c3269d73674c90430ed617aab4c738
Kamal <atluxanovvkamal@gmail.com>
2025-11-15 17:06:28 +0000

    shell

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 #salam
-str1 = "Welcome to"
-str2 = " Holberton School!"
-print(str1 + str2)
+str1 = "Holberton"
+str2 = "School"
+full_name = f"{str1} {str2}"
+print(f"Welcome to {full_name}!")

72024e45ea4b9903b0fbe33bdd0ea61a1cf31f0b
Kamal <atluxanovvkamal@gmail.com>
2025-11-15 16:37:11 +0000

    shell

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    print("{}".format(chr(122 - i) if i % 2 == 0 else chr(90 - i)), end="")
+print("".join(chr(i) if i % 2 == 0 else chr(i - 32) for i in range(122, 96, -1)), end="")
+

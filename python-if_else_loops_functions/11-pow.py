f6b52f01aa4bd0af3cc4c185acbfc4776aa625bb
Kamal <atluxanovvkamal@gmail.com>
2025-11-15 13:10:38 +0000

    shell

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def pow(a, b):
     result = 1
-    for _ in range(b):
+    exponent = abs(b)  # müsbət dərəcəyə çevir
+    for _ in range(exponent):
         result *= a
+    if b < 0:
+        return 1 / result  # mənfi qüvvə üçün tərsini qaytar
     return result

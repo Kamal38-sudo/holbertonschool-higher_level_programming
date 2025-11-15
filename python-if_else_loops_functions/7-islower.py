fd9d0dc40012a01334329d214bf265fd488ad89b
Kamal <atluxanovvkamal@gmail.com>
2025-11-15 08:35:58 +0000

    shell

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,12 +1,10 @@
- #!/usr/bin/python3
-
+#!/usr/bin/python3
 def islower(c):
     """
     Returns True if c is a lowercase character, False otherwise.
     """
     return ord('a') <= ord(c) <= ord('z')
-
-print(islower("a"))  
-print(islower("H"))  
-print(islower("3"))  
+print(islower("a"))
+print(islower("H"))
+print(islower("3"))
 print(islower("g"))

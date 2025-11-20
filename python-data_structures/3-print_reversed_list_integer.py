f5bfdf065833470894c75136cec68deae040d1e6
Kamal <atluxanovvkamal@gmail.com>
2025-11-20 11:34:06 +0000

    shell

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """Print all integers of a list in reverse order, one per line using str.format()"""
-    for num in my_list[::-1]:  # listi tərs sırayla götürür
+    for num in my_list[::-1]:
         print("{:d}".format(num))

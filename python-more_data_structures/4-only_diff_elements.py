cd470139e1acb7c74dfe39e43699292376d4a62b
Kamal <atluxanovvkamal@gmail.com>
2025-11-21 08:29:05 +0000

    shell

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,12 +2,12 @@
 def only_diff_elements(set_1, set_2):
     result = set()
 
-    for element in set_1:
-        if element not in set_2:
-            result.add(element)
+    for x in set_1:
+        if x not in set_2:
+            result.add(x)
 
-    for element in set_2:
-        if element not in set_1:
-            result.add(element)
+    for x in set_2:
+        if x not in set_1:
+            result.add(x)
 
     return result

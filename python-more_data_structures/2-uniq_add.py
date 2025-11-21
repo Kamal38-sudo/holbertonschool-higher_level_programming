383e91839cfa7c076264af1a999f6cfcfd0d1cb5
Kamal <atluxanovvkamal@gmail.com>
2025-11-21 06:35:51 +0000

    shell

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique = []
+    total = 0
+
+    for x in my_list:
+        if x not in unique:
+            unique.append(x)
+            total += x
+
+    return total
+

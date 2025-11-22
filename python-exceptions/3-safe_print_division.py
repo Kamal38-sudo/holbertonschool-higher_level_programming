8c0dd3f49530d30ed9a01de176d31c33134d892e
Kamal <atluxanovvkamal@gmail.com>
2025-11-22 11:19:02 +0000

    shell

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
     result = None
     try:
@@ -9,4 +8,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
     return result
-

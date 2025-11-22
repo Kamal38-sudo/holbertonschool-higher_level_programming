637575a9744f3c4f57ff0eea006ca3aa592d623c
Kamal <atluxanovvkamal@gmail.com>
2025-11-22 11:54:48 +0000

    shell

--- python-exceptions/4-main.py
+++ python-exceptions/4-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-list_division = __import__('4_list_division').list_division
+list_division = __import__('4-list_division').list_division
 my_l_1 = [10, 8, 4]
 my_l_2 = [2, 4, 4]
 result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))

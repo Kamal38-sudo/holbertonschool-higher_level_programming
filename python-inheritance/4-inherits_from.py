0c3ea5b99cf696d9dfa32238833bb297b4e3ca23
Kamal <atluxanovvkamal@gmail.com>
2025-11-28 12:10:54 +0000

    shell

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Module that checks if an object inherits from a class (directly or indirectly)."""
+
+
+"""salam"""
 
 
 def inherits_from(obj, a_class):
-    """
-    Return True if obj is an instance of a class that inherited
-    (directly or indirectly) from a_class.
+    """Return True if s;
+    otherwise False.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class

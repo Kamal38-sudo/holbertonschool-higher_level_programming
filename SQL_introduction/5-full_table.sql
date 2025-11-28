db0125a787318dd4c10fbd357b4bc3d58cbd9f43
Kamal <atluxanovvkamal@gmail.com>
2025-11-28 15:35:55 +0000

    shell

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,10 +1,6 @@
 -- salam
-SELECT column_name AS Field,
-       column_type AS Type,
-       is_nullable AS `Null`,
-       column_key AS `Key`,
-       column_default AS `Default`,
-       extra AS Extra
-FROM information_schema.COLUMNS
-WHERE table_schema = DATABASE()  -- cari baza
-  AND table_name = 'first_table';
+CREATE TABLE `first_table` (
+  `id` int DEFAULT NULL,
+  `name` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+

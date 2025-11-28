51331372696fdd36f0c015332fe5e31993b8a74f
Kamal <atluxanovvkamal@gmail.com>
2025-11-28 12:45:03 +0000

    shell

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with JSON serialization and optional filtering."""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance with first_name, last_name, and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of the Student instance.
+        If attrs is a list of strings, only attributes in this list are retrieved.
+        Otherwise, return all attributes.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__.copy()

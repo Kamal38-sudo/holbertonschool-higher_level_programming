8b3d2f6a223e8e37015482f966237f263fcfac03
Kamal <atluxanovvkamal@gmail.com>
2025-11-28 16:48:29 +0000

    shell

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -8,8 +8,9 @@ ALTER TABLE first_table
     CONVERT TO CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 
--- Convert column to UTF8
+-- Convert column name to UTF8
 ALTER TABLE first_table
     MODIFY name VARCHAR(256)
     CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
+

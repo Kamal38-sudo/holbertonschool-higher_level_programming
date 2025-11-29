7b36646f0b43e14bef731b7fb870f319c8586da5
Kamal <atluxanovvkamal@gmail.com>
2025-11-29 10:49:48 +0000

    shell

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,8 @@
--- salam
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
--- salam
+-- user_0d_1 yarat və root privileges ver
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
--- salam
 FLUSH PRIVILEGES;
+
+-- privileges-i yoxla
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+

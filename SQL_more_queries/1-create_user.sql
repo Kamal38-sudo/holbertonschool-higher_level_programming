1326a54148de6b5cbfaa61fc0cbab3ea00effb7f
Kamal <atluxanovvkamal@gmail.com>
2025-11-29 10:57:38 +0000

    shell

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,11 +1,9 @@
 -- 1-create_user.sql
-
--- 1. user_0d_1 mövcud deyilsə yaradılır
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- 2. Bütün privileges verilir (root səviyyəsində)
+-- salam
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
--- 3. Dəyişiklikləri dərhal aktivləşdir
+-- salam
 FLUSH PRIVILEGES;
 

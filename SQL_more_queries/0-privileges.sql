f3fc4a2c1a1e11974b8aef041191b01fbfdd1234
Kamal <atluxanovvkamal@gmail.com>
2025-11-29 10:15:17 +0000

    shell

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- salam
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY '1234';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY '1234';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'loclahost';

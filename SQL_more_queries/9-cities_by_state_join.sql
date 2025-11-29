d8cf287238af1d4972bcf62b5c1892a4301a659b
Kamal <atluxanovvkamal@gmail.com>
2025-11-29 15:07:26 +0000

    shell

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- salam
-SELECT cities.id, cities.name AS city, states.name AS state
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
-

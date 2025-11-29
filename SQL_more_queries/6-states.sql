d002396c2a48e9926a64c48915db49ff2d6cf902
Kamal <atluxanovvkamal@gmail.com>
2025-11-29 11:26:29 +0000

    shell

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,13 @@
--- salam
+-- 1. Verilənlər bazasını yaradın (mövcuddursa error verməsin)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- salam
+
+-- 2. Verilənlər bazasını istifadə edin
+USE hbtn_0d_usa;
+
+-- 3. states cədvəlini yaradın (mövcuddursa error verməsin)
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
+

0a5873c984beb20d3c2332458abc1b2cb2ca345c
Kamal <atluxanovvkamal@gmail.com>
2025-11-29 10:52:05 +0000

    shell

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,11 @@
--- user_0d_1 yarat və root privileges ver
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+-- 1-create_user.sql
 
--- privileges-i yoxla
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- 1. user_0d_1 mövcud deyilsə yaradılır
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- 2. Bütün privileges verilir (root səviyyəsində)
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- 3. Dəyişiklikləri dərhal aktivləşdir
+FLUSH PRIVILEGES;
 

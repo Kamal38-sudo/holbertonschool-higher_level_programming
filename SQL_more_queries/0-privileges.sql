f1b32ebfd5587bb3937074f57ec30bf98c62a58c
Kamal <atluxanovvkamal@gmail.com>
2025-11-29 10:33:39 +0000

    shell

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,11 @@
--- salam
+-- ÆYarat
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'password123';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'password123';
+
+-- Icaze ver
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+
+-- Goster
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'loclahost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

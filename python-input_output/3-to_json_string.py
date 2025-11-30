1eae2448ad7617f60ec1a069721029288952fc57
Kamal <atluxanovvkamal@gmail.com>
2025-11-30 15:02:02 +0000

    shell

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Module that defines a function to convert an object to its JSON string representation."""
+"""salam"""
+
 
 import json
 
 
 def to_json_string(my_obj):
-    """Return the JSON representation (string) of an object."""
+    """Reton of a Python object (string)."""
     return json.dumps(my_obj)

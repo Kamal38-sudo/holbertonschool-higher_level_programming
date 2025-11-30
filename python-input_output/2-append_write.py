6a49d31d69ca7003890156aef9451ed0f0f54d31
Kamal <atluxanovvkamal@gmail.com>
2025-11-30 15:00:58 +0000

    shell

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Module that defines a function to append a string to a UTF-8 text file."""
+"""salam"""
 
 
 def append_write(filename="", text=""):
-    """Append a string to the end of a text file (UTF-8) and return the number of characters added."""
+    """salam"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)

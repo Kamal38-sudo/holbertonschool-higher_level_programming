147bfbcb8ba546607217ce1268f91e0dfc62ce1a
Kamal <atluxanovvkamal@gmail.com>
2025-11-30 15:38:35 +0000

    shell

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,16 +1,15 @@
--- Convert database to UTF8
+-- Convert the database to utf8mb4
 ALTER DATABASE hbtn_0c_0
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
--- Convert table to UTF8
+-- Convert the table to utf8mb4
 ALTER TABLE first_table
-    CONVERT TO CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
--- Convert column name to UTF8
+-- Convert the 'name' column to utf8mb4
 ALTER TABLE first_table
-    MODIFY name VARCHAR(256)
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
-
+MODIFY name VARCHAR(256) 
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;

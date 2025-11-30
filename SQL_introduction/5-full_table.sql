d1bae7676611014657749d92cf6feb3525815157
Kamal <atluxanovvkamal@gmail.com>
2025-11-30 14:48:02 +0000

    shell

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,2 @@
 -- salam
-CREATE TABLE `first_table` (
-  `id` int DEFAULT NULL,
-  `name` varchar(256) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-
+SHOW CREATE TABLE first_table;

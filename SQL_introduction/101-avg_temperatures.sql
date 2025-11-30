19922c2ac6cea92b35107418d3984311be3116e5
Kamal <atluxanovvkamal@gmail.com>
2025-11-30 15:43:09 +0000

    shell

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Display the average temperature by city ordered descending
-SELECT city, ROUND(AVG(temperature), 4) AS avg_temp
-FROM temperatures
+SELECT city, ROUND(AVG(temp), 4) AS avg_temp
+FROM city_temperature_table
 GROUP BY city
 ORDER BY avg_temp DESC;

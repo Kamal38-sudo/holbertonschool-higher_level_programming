04eeac02c44d397d4aa02727a576a32c3eb2f3fc
Kamal <atluxanovvkamal@gmail.com>
2025-12-02 10:35:08 +0000

    shell

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
-"""This task is urllib."""
+"""
+1-hbtn_header.py
+
+This script takes in a URL, sends a HTTP request to the URL,
+and displays the value of the X-Request-Id variable found in
+the header of the response.
+
+Usage:
+    ./1-hbtn_header.py <URL>
+
+Example:
+    ./1-hbtn_header.py https://intranet.hbtn.io
+"""
 
 import sys
 import urllib.request
@@ -7,6 +19,7 @@ import urllib.request
 url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
+    # Get the X-Request-Id header
+    x_request_id = response.getheader("X-Request-Id")
+    print(x_request_id)
 
-x_request_id = response.getheader("x-request-id")
-print(x_request_id)

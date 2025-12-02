f445703ce22b858be63a7be88d404586e7f45c77
Kamal <atluxanovvkamal@gmail.com>
2025-12-02 10:39:19 +0000

    shell

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,15 +2,25 @@
 """
 1-hbtn_header.py
 
-This script takes in a URL, sends a HTTP request to the URL,
-and displays the value of the X-Request-Id variable found in
-the header of the response.
+This script takes in a URL, sends a request to the URL, and displays the
+value of the X-Request-Id variable found in the header of the response.
+
+Requirements:
+- Must use the packages urllib and sys
+- Cannot import packages other than urllib and sys
+- The value of X-Request-Id is different for each request
+- No need to check arguments passed to the script (number or type)
+- Must use a with statement
 
 Usage:
     ./1-hbtn_header.py <URL>
 
 Example:
-    ./1-hbtn_header.py https://intranet.hbtn.io
+    $ ./1-hbtn_header.py https://intranet.hbtn.io
+    ade2627e-41dd-4c34-b9d9-a0fa0f47b237
+
+    $ ./1-hbtn_header.py https://intranet.hbtn.io
+    6593e1f5-1b4b-4c9f-9c0e-72ab525b850f
 """
 
 import sys
@@ -19,7 +29,5 @@ import urllib.request
 url = sys.argv[1]
 
 with urllib.request.urlopen(url) as response:
-    # Get the X-Request-Id header
     x_request_id = response.getheader("X-Request-Id")
     print(x_request_id)
-

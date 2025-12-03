2c96148ae8aaf4ef678d0cc2138756d1c79b29f0
Kamal <atluxanovvkamal@gmail.com>
2025-12-03 11:55:49 +0000

    network

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -9,5 +9,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    x = response.header.get("X-Request-Id")
+    x = response.headers.get("X-Request-Id")
     print(x)

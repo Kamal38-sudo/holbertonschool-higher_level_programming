ae4966c0d99349e4b952691817bbae0d0bbc263d
Kamal <atluxanovvkamal@gmail.com>
2025-12-03 11:38:31 +0000

    network

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,4 +8,6 @@ if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     response = requests.get(url)
     body = response.text
-    print(body)
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
